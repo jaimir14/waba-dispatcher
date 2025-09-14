46893ad9e0e8f73c824159251e1cee658ae75e34
Jairo Miranda <jaimir14@gmail.com>
2025-09-14 16:33:26 -0600

    Update expires at

--- src/database/repositories/conversation.repository.ts
+++ src/database/repositories/conversation.repository.ts
@@ -158,7 +158,7 @@ export class ConversationRepository {
    */
   async updateSessionExpiration(phoneNumber: string): Promise<void> {
     const expiresAt = new Date();
-    expiresAt.setHours(expiresAt.getHours() + 24);
+    expiresAt.setHours(expiresAt.getHours() + 24 + 6); // 6 hours more than 24 hours because of the timezone difference
 
     await Conversation.update(
       {
@@ -166,7 +166,7 @@ export class ConversationRepository {
         last_message_at: new Date(),
       },
       {
-        where: { phone_number: phoneNumber},
+        where: { phone_number: phoneNumber },
       },
     );
   }
